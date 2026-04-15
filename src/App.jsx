eaaf5e872d60b64736d83db3229c6b0fd69a5888
Михаил Королев <mihail_korolev1111@mail.ru>
2026-04-15 21:08:46 +0300

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import one from '../public/policy_page-0001.jpg'
-import two from '../public/policy_page-0002.jpg'
-import three from '../public/policy_page-0003.jpg'
+import one from './assets/policy_page-0001.jpg'
+import two from './assets/policy_page-0002.jpg'
+import three from './assets/policy_page-0003.jpg'
 import './App.css'
 
 function App() {
