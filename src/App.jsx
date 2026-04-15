6179dd80938560133d06b67013101620f833b676
Михаил Королев <mihail_korolev1111@mail.ru>
2026-04-15 20:59:04 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import one from '../public/policy_page-0001.jpg'
+import two from '../public/policy_page-0002.jpg'
+import three from '../public/policy_page-0003.jpg'
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <img src={one}/>
+            <img src={two}/>
+            <img src={three}/>
+        </>
+    )
+}
+
+export default App
